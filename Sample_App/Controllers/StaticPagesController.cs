a2a8af4269eb4f7c3eff5934a28a3396b5eee719
Luke Haites <Luke.Haites@Apparel21.com>
2013-08-22 14:46:58 +1000

    First Commit w Static Pages

---
+++ Sample_App/Controllers/StaticPagesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample_App.Controllers
+{
+    public class StaticPagesController : Controller
+    {
+        //
+        // GET: /StaticPages/
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Help()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
